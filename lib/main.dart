03acfff01672a24689a5f5ece41ee92e573397aa
Vincent REMY <vincentremy222@gmail.com>
2023-07-08 16:08:03 +0200

    feat : add dotenv support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/router/router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env.local");
   runApp(
       /*MultiProvider(
     providers: [
