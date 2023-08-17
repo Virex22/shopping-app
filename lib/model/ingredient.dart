e459dc9a4abcfeaf41ec108cca5de5ba720df422
Vincent REMY <vincentremy222@gmail.com>
2023-08-17 11:27:09 +0200

    feat : add ingredient model

---
+++ lib/model/ingredient.dart
@@ -0,0 +1,59 @@
+import 'package:shopping_app/model/product.dart';
+
+class Ingredient {
+  static String getIrifromId(int id) {
+    return '/api/ingredients/$id';
+  }
+
+  int id;
+  Product? product;
+  String quantity;
+  String? customName;
+  double? customPrice;
+  String quantityType;
+
+  get iri => Ingredient.getIrifromId(id);
+
+  Ingredient({
+    required this.id,
+    required this.product,
+    required this.quantity,
+    required this.customName,
+    required this.customPrice,
+    required this.quantityType,
+  });
+
+  get name {
+    if (product == null && customName != null) {
+      return customName;
+    } else if (product != null) {
+      return product!.name;
+    } else {
+      return '';
+    }
+  }
+
+  get price {
+    if (product == null && customPrice != null) {
+      return customPrice;
+    } else if (product != null) {
+      return product!.price;
+    } else {
+      return '';
+    }
+  }
+
+  factory Ingredient.fromJson(Map<String, dynamic> json) {
+    return Ingredient(
+      id: json['id'],
+      product:
+          json['product'] == null ? null : Product.fromJson(json['product']),
+      quantity: json['quantity'],
+      customName: json['customName'],
+      customPrice: json['customPrice'] == null
+          ? null
+          : double.parse(json['customPrice']),
+      quantityType: json['quantityType'],
+    );
+  }
+}
