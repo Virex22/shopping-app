1c96d0bc5bac8209b7aa05fa1ec7e7b2c926dc8d
Vincent REMY <vincentremy222@gmail.com>
2023-08-17 11:29:18 +0200

    feat : add price history model

---
+++ lib/model/price_history.dart
@@ -0,0 +1,39 @@
+/*
+[
+  {
+    "id": 0,
+    "product": "string",
+    "price": "string",
+    "dateUpdate": "2023-08-17T09:27:45.211Z"
+  }
+]
+*/
+
+class PriceHistory {
+  static String getIrifromId(int id) {
+    return '/api/price_histories/$id';
+  }
+
+  int id;
+  String productURI;
+  double price;
+  DateTime dateUpdate;
+
+  get iri => PriceHistory.getIrifromId(id);
+
+  PriceHistory({
+    required this.id,
+    required this.productURI,
+    required this.price,
+    required this.dateUpdate,
+  });
+
+  factory PriceHistory.fromJson(Map<String, dynamic> json) {
+    return PriceHistory(
+      id: json['id'],
+      productURI: json['product'],
+      price: double.parse(json['price']),
+      dateUpdate: DateTime.parse(json['dateUpdate']),
+    );
+  }
+}
